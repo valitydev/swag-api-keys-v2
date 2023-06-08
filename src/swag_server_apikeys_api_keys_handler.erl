06febd46dceee06633f34af82dfe798b34fecf09
ndiezel0 <ndiezel0@gmail.com>
2023-06-08 07:50:15 +0000

    Release valitydev/swag-api-keys-v2@73d97d2d067cf525650e0067f5e9124866f6e732

---
+++ src/swag_server_apikeys_api_keys_handler.erl
@@ -0,0 +1,592 @@
+%% -*- mode: erlang -*-
+
+%% basic handler
+-module(swag_server_apikeys_api_keys_handler).
+
+%% Cowboy REST callbacks
+-export([allowed_methods/2]).
+-export([init/2]).
+-export([allow_missing_post/2]).
+-export([content_types_accepted/2]).
+-export([content_types_provided/2]).
+-export([charsets_provided/2]).
+-export([delete_resource/2]).
+-export([is_authorized/2]).
+-export([known_content_type/2]).
+-export([malformed_request/2]).
+-export([valid_content_headers/2]).
+-export([valid_entity_length/2]).
+
+%% Handlers
+-export([handle_request_json/2]).
+
+-record(state, {
+    operation_id  :: swag_server_apikeys:operation_id(),
+    logic_handler :: module(),
+    swagger_handler_opts :: swag_server_apikeys_router:swagger_handler_opts(),
+    context       :: swag_server_apikeys:request_context()
+}).
+
+-type state()              :: state().
+-type content_type()       :: {binary(), binary(), '*' | [{binary(), binary()}]}.
+-type processed_response() :: {stop, cowboy_req:req(), state()}.
+
+%% Cowboy REST callbacks
+
+-spec init(Req :: cowboy_req:req(), Opts :: swag_server_apikeys_router:init_opts()) ->
+    {cowboy_rest, Req :: cowboy_req:req(), State :: state()}.
+
+init(Req, {_Operations, LogicHandler, SwaggerHandlerOpts} = InitOpts) ->
+    OperationID    = swag_server_apikeys_utils:get_operation_id(Req, InitOpts),
+
+    error_logger:info_msg("Attempt to process operation: ~p", [OperationID]),
+
+    State = #state{
+        operation_id  = OperationID,
+        logic_handler = LogicHandler,
+        swagger_handler_opts = SwaggerHandlerOpts,
+        context       = #{cowboy_req => Req}
+    },
+    {cowboy_rest, Req, State}.
+
+-spec allowed_methods(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: [binary()], Req :: cowboy_req:req(), State :: state()}.
+
+
+allowed_methods(
+    Req,
+    State = #state{
+        operation_id = 'GetApiKey'
+    }
+) ->
+    {[<<"GET">>], Req, State};
+
+allowed_methods(
+    Req,
+    State = #state{
+        operation_id = 'IssueApiKey'
+    }
+) ->
+    {[<<"POST">>], Req, State};
+
+allowed_methods(
+    Req,
+    State = #state{
+        operation_id = 'ListApiKeys'
+    }
+) ->
+    {[<<"GET">>], Req, State};
+
+allowed_methods(
+    Req,
+    State = #state{
+        operation_id = 'RequestRevokeApiKey'
+    }
+) ->
+    {[<<"PUT">>], Req, State};
+
+allowed_methods(
+    Req,
+    State = #state{
+        operation_id = 'RevokeApiKey'
+    }
+) ->
+    {[<<"GET">>], Req, State};
+
+allowed_methods(Req, State) ->
+    {[], Req, State}.
+
+-spec is_authorized(Req :: cowboy_req:req(), State :: state()) ->
+    {
+        Value :: true | {false, AuthHeader :: iodata()},
+        Req   :: cowboy_req:req(),
+        State :: state()
+    }.
+
+is_authorized(
+    Req0,
+    State = #state{
+        operation_id  = 'GetApiKey' = OperationID,
+        logic_handler = LogicHandler,
+        context       = Context
+    }
+) ->
+    From = header,
+    Result = swag_server_apikeys_handler_api:authorize_api_key(
+        LogicHandler,
+        OperationID,
+        From,
+        'Authorization',
+        Req0,
+        Context
+    ),
+    case Result of
+        {true, AuthContext, Req} ->
+            NewContext = Context#{
+                auth_context => AuthContext
+            },
+            {true, Req, State#state{context = NewContext}};
+        {false, AuthHeader, Req} ->
+            {{false, AuthHeader}, Req, State}
+    end;
+
+is_authorized(
+    Req0,
+    State = #state{
+        operation_id  = 'IssueApiKey' = OperationID,
+        logic_handler = LogicHandler,
+        context       = Context
+    }
+) ->
+    From = header,
+    Result = swag_server_apikeys_handler_api:authorize_api_key(
+        LogicHandler,
+        OperationID,
+        From,
+        'Authorization',
+        Req0,
+        Context
+    ),
+    case Result of
+        {true, AuthContext, Req} ->
+            NewContext = Context#{
+                auth_context => AuthContext
+            },
+            {true, Req, State#state{context = NewContext}};
+        {false, AuthHeader, Req} ->
+            {{false, AuthHeader}, Req, State}
+    end;
+
+is_authorized(
+    Req0,
+    State = #state{
+        operation_id  = 'ListApiKeys' = OperationID,
+        logic_handler = LogicHandler,
+        context       = Context
+    }
+) ->
+    From = header,
+    Result = swag_server_apikeys_handler_api:authorize_api_key(
+        LogicHandler,
+        OperationID,
+        From,
+        'Authorization',
+        Req0,
+        Context
+    ),
+    case Result of
+        {true, AuthContext, Req} ->
+            NewContext = Context#{
+                auth_context => AuthContext
+            },
+            {true, Req, State#state{context = NewContext}};
+        {false, AuthHeader, Req} ->
+            {{false, AuthHeader}, Req, State}
+    end;
+
+is_authorized(
+    Req0,
+    State = #state{
+        operation_id  = 'RequestRevokeApiKey' = OperationID,
+        logic_handler = LogicHandler,
+        context       = Context
+    }
+) ->
+    From = header,
+    Result = swag_server_apikeys_handler_api:authorize_api_key(
+        LogicHandler,
+        OperationID,
+        From,
+        'Authorization',
+        Req0,
+        Context
+    ),
+    case Result of
+        {true, AuthContext, Req} ->
+            NewContext = Context#{
+                auth_context => AuthContext
+            },
+            {true, Req, State#state{context = NewContext}};
+        {false, AuthHeader, Req} ->
+            {{false, AuthHeader}, Req, State}
+    end;
+
+is_authorized(
+    Req0,
+    State = #state{
+        operation_id  = 'RevokeApiKey' = OperationID,
+        logic_handler = LogicHandler,
+        context       = Context
+    }
+) ->
+    From = header,
+    Result = swag_server_apikeys_handler_api:authorize_api_key(
+        LogicHandler,
+        OperationID,
+        From,
+        'Authorization',
+        Req0,
+        Context
+    ),
+    case Result of
+        {true, AuthContext, Req} ->
+            NewContext = Context#{
+                auth_context => AuthContext
+            },
+            {true, Req, State#state{context = NewContext}};
+        {false, AuthHeader, Req} ->
+            {{false, AuthHeader}, Req, State}
+    end;
+
+is_authorized(Req, State) ->
+    {{false, <<"">>}, Req, State}.
+
+-spec content_types_accepted(Req :: cowboy_req:req(), State :: state()) ->
+    {
+        Value :: [{content_type(), AcceptResource :: atom()}],
+        Req   :: cowboy_req:req(),
+        State :: state()
+    }.
+
+content_types_accepted(Req, State) ->
+    {[
+        {{<<"application">>, <<"json">>, [{<<"charset">>, <<"utf-8">>}]}, handle_request_json}
+    ], Req, State}.
+
+-spec valid_content_headers(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: boolean(), Req :: cowboy_req:req(), State :: state()}.
+
+valid_content_headers(
+    Req0,
+    State = #state{
+        operation_id = 'GetApiKey'
+    }
+) ->
+    Headers = ["X-Request-ID","X-Request-Deadline"],
+    {Result, Req} = validate_headers(Headers, Req0),
+    {Result, Req, State};
+
+valid_content_headers(
+    Req0,
+    State = #state{
+        operation_id = 'IssueApiKey'
+    }
+) ->
+    Headers = [],
+    {Result, Req} = validate_headers(Headers, Req0),
+    {Result, Req, State};
+
+valid_content_headers(
+    Req0,
+    State = #state{
+        operation_id = 'ListApiKeys'
+    }
+) ->
+    Headers = ["X-Request-ID","X-Request-Deadline"],
+    {Result, Req} = validate_headers(Headers, Req0),
+    {Result, Req, State};
+
+valid_content_headers(
+    Req0,
+    State = #state{
+        operation_id = 'RequestRevokeApiKey'
+    }
+) ->
+    Headers = ["X-Request-ID","X-Request-Deadline"],
+    {Result, Req} = validate_headers(Headers, Req0),
+    {Result, Req, State};
+
+valid_content_headers(
+    Req0,
+    State = #state{
+        operation_id = 'RevokeApiKey'
+    }
+) ->
+    Headers = ["X-Request-ID","X-Request-Deadline"],
+    {Result, Req} = validate_headers(Headers, Req0),
+    {Result, Req, State};
+
+valid_content_headers(Req, State) ->
+    {false, Req, State}.
+
+-spec content_types_provided(Req :: cowboy_req:req(), State :: state()) ->
+    {
+        Value :: [{content_type(), ProvideResource :: atom()}],
+        Req   :: cowboy_req:req(),
+        State :: state()
+    }.
+
+content_types_provided(Req, State) ->
+    {[
+        {{<<"application">>, <<"json">>, '*'}, handle_request_json}
+    ], Req, State}.
+
+-spec charsets_provided(Req :: cowboy_req:req(), State :: state()) ->
+    {Charsets :: [binary()], Req :: cowboy_req:req(), State :: state()}.
+
+charsets_provided(Req, State) ->
+    {[<<"utf-8">>], Req, State}.
+
+-spec malformed_request(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: boolean(), Req :: cowboy_req:req(), State :: state()}.
+
+malformed_request(Req, State) ->
+    {false, Req, State}.
+
+-spec allow_missing_post(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: false, Req :: cowboy_req:req(), State :: state()}.
+
+allow_missing_post(Req, State) ->
+    {false, Req, State}.
+
+-spec delete_resource(Req :: cowboy_req:req(), State :: state()) ->
+    processed_response().
+
+delete_resource(Req, State) ->
+    handle_request_json(Req, State).
+
+-spec known_content_type(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: true, Req :: cowboy_req:req(), State :: state()}.
+
+known_content_type(Req, State) ->
+    {true, Req, State}.
+
+-spec valid_entity_length(Req :: cowboy_req:req(), State :: state()) ->
+    {Value :: true, Req :: cowboy_req:req(), State :: state()}.
+
+valid_entity_length(Req, State) ->
+    %% @TODO check the length
+    {true, Req, State}.
+
+
+%% Handlers
+
+-spec handle_request_json(Req :: cowboy_req:req(), State :: state()) ->
+    processed_response().
+
+handle_request_json(
+    Req0,
+    State = #state{
+        operation_id  = OperationID,
+        logic_handler = LogicHandler,
+        swagger_handler_opts = SwaggerHandlerOpts,
+        context       = Context
+    }
+) ->
+    ValidationOpts = maps:get(validation_opts, SwaggerHandlerOpts, #{}),
+    case populate_request(LogicHandler, OperationID, Req0, ValidationOpts) of
+        {ok, Populated, Req1} ->
+            {Status, Resp} = handle_request(LogicHandler, OperationID, Populated, Context),
+            ok = validate_response(Status, Resp, OperationID, ValidationOpts),
+            process_response(ok, encode_response(Resp), Req1, State);
+        {error, Reason, Req1} ->
+            process_response(error, Reason, Req1, State)
+    end.
+
+
+%% Internal
+
+populate_request(LogicHandler, OperationID, Req, ValidationOpts) ->
+    Spec = get_request_spec(OperationID),
+    swag_server_apikeys_handler_api:populate_request(LogicHandler, OperationID, Spec, Req, ValidationOpts).
+
+handle_request(LogicHandler, OperationID, Populated, Context) ->
+    swag_server_apikeys_logic_handler:handle_request(LogicHandler, OperationID, Populated, Context).
+
+validate_response(error, _, _, _) ->
+    ok;
+validate_response(ok, {Code, _Headers, Body}, OperationID, ValidationOpts) ->
+    Spec = get_response_spec(OperationID, Code),
+    swag_server_apikeys_handler_api:validate_response(OperationID, Spec, Body, ValidationOpts).
+
+encode_response(Resp) ->
+    swag_server_apikeys_handler_api:encode_response(Resp).
+
+process_response(Status, Result, Req0, State = #state{operation_id = OperationID}) ->
+    Req = swag_server_apikeys_handler_api:process_response(Status, Result, Req0, OperationID),
+    {stop, Req, State}.
+
+validate_headers(_, Req) ->
+    {true, Req}.
+
+-spec get_request_spec(OperationID :: swag_server_apikeys:operation_id()) ->
+    Spec :: swag_server_apikeys_handler_api:request_spec() | no_return().
+
+
+get_request_spec('GetApiKey') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'apiKeyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('IssueApiKey') ->
+    [
+        {'partyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'ApiKey', #{
+            source => body,
+            rules  => [schema, {required, true}]
+        }}
+    ];
+get_request_spec('ListApiKeys') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }},
+        {'status', #{
+            source => qs_val,
+            rules  => [{type, 'binary'}, {enum, ['Active', 'Revoked']}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('RequestRevokeApiKey') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'apiKeyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'binary', #{
+            source => body,
+            rules  => [schema, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ];
+get_request_spec('RevokeApiKey') ->
+    [
+        {'X-Request-ID', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 32}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'partyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'apiKeyId', #{
+            source => binding,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'apiKeyRevokeToken', #{
+            source => qs_val,
+            rules  => [{type, 'binary'}, {max_length, 4000}, {min_length, 1}, true
+, {required, true}]
+        }},
+        {'X-Request-Deadline', #{
+            source => header,
+            rules  => [{type, 'binary'}, {max_length, 40}, {min_length, 1}, true
+, {required, false}]
+        }}
+    ].
+
+-spec get_response_spec(OperationID :: swag_server_apikeys:operation_id(), Code :: cowboy:http_status()) ->
+    Spec :: swag_server_apikeys_handler_api:response_spec() | no_return().
+
+
+get_response_spec('GetApiKey', 200) ->
+    {'ApiKey', 'ApiKey'};
+
+get_response_spec('GetApiKey', 400) ->
+    {'BadRequest', 'BadRequest'};
+
+get_response_spec('GetApiKey', 401) ->
+    undefined;
+
+get_response_spec('GetApiKey', 404) ->
+    undefined;
+
+get_response_spec('IssueApiKey', 200) ->
+    {'inline_response_200_1', 'inline_response_200_1'};
+
+get_response_spec('IssueApiKey', 400) ->
+    {'BadRequest', 'BadRequest'};
+
+get_response_spec('IssueApiKey', 401) ->
+    undefined;
+
+get_response_spec('ListApiKeys', 200) ->
+    {'inline_response_200', 'inline_response_200'};
+
+get_response_spec('ListApiKeys', 400) ->
+    {'BadRequest', 'BadRequest'};
+
+get_response_spec('ListApiKeys', 401) ->
+    undefined;
+
+get_response_spec('ListApiKeys', 404) ->
+    undefined;
+
+get_response_spec('RequestRevokeApiKey', 204) ->
+    undefined;
+
+get_response_spec('RequestRevokeApiKey', 400) ->
+    {'BadRequest', 'BadRequest'};
+
+get_response_spec('RequestRevokeApiKey', 401) ->
+    undefined;
+
+get_response_spec('RequestRevokeApiKey', 404) ->
+    undefined;
+
+get_response_spec('RevokeApiKey', 204) ->
+    undefined;
+
+get_response_spec('RevokeApiKey', 400) ->
+    {'BadRequest', 'BadRequest'};
+
+get_response_spec('RevokeApiKey', 401) ->
+    undefined;
+
+get_response_spec('RevokeApiKey', 404) ->
+    undefined;
+
+get_response_spec(OperationID, Code) ->
+    error({invalid_response_code, OperationID, Code}).
---
+++ src/swag_server_apikeys_router.erl
@@ -0,0 +1,118 @@
+-module(swag_server_apikeys_router).
+
+-export([get_paths/1]).
+-export([get_paths/2]).
+-export([get_operation/1]).
+-export([get_operations/0]).
+
+-type operations() :: #{
+    Method :: binary() => OperationID :: swag_server_apikeys:operation_id()
+}.
+
+-type logic_handler(T) :: swag_server_apikeys:logic_handler(T).
+
+-type swagger_handler_opts() :: #{
+    validation_opts => swag_server_apikeys_validation:validation_opts()
+}.
+
+-type init_opts() :: {
+    Operations      :: operations(),
+    LogicHandler    :: logic_handler(_),
+    SwaggerHandlerOpts :: swagger_handler_opts()
+}.
+
+-type operation_spec() :: #{
+    path    := '_' | iodata(),
+    method  := binary(),
+    handler := module()
+}.
+
+-export_type([swagger_handler_opts/0]).
+-export_type([init_opts/0]).
+-export_type([operation_spec/0]).
+
+-spec get_paths(LogicHandler :: logic_handler(_)) ->  [{'_',[{
+    Path :: string(),
+    Handler :: atom(),
+    InitOpts :: init_opts()
+}]}].
+
+get_paths(LogicHandler) ->
+    get_paths(LogicHandler, #{}).
+
+-spec get_paths(LogicHandler :: logic_handler(_), SwaggerHandlerOpts :: swagger_handler_opts()) ->  [{'_',[{
+    Path :: string(),
+    Handler :: atom(),
+    InitOpts :: init_opts()
+}]}].
+
+get_paths(LogicHandler, SwaggerHandlerOpts) ->
+    PreparedPaths = maps:fold(
+        fun(Path, #{operations := Operations, handler := Handler}, Acc) ->
+            [{Path, Handler, Operations} | Acc]
+        end,
+        [],
+        group_paths()
+    ),
+    [
+        {'_',
+            [{P, H, {O, LogicHandler, SwaggerHandlerOpts}} || {P, H, O} <- PreparedPaths]
+        }
+    ].
+
+group_paths() ->
+    maps:fold(
+        fun(OperationID, #{path := Path, method := Method, handler := Handler}, Acc) ->
+            case maps:find(Path, Acc) of
+                {ok, PathInfo0 = #{operations := Operations0}} ->
+                    Operations = Operations0#{Method => OperationID},
+                    PathInfo = PathInfo0#{operations => Operations},
+                    Acc#{Path => PathInfo};
+                error ->
+                    Operations = #{Method => OperationID},
+                    PathInfo = #{handler => Handler, operations => Operations},
+                    Acc#{Path => PathInfo}
+            end
+        end,
+        #{},
+        get_operations()
+    ).
+
+-spec get_operation(swag_server_apikeys:operation_id()) ->
+   operation_spec().
+
+get_operation(OperationId) ->
+    maps:get(OperationId, get_operations()).
+
+-spec get_operations() -> #{
+    swag_server_apikeys:operation_id() := operation_spec()
+}.
+
+get_operations() ->
+    #{ 
+        'GetApiKey' => #{
+            path => "/apikeys/v2/orgs/:partyId/api-keys/:apiKeyId",
+            method => <<"GET">>,
+            handler => 'swag_server_apikeys_api_keys_handler'
+        },
+        'IssueApiKey' => #{
+            path => "/apikeys/v2/orgs/:partyId/api-keys",
+            method => <<"POST">>,
+            handler => 'swag_server_apikeys_api_keys_handler'
+        },
+        'ListApiKeys' => #{
+            path => "/apikeys/v2/orgs/:partyId/api-keys",
+            method => <<"GET">>,
+            handler => 'swag_server_apikeys_api_keys_handler'
+        },
+        'RequestRevokeApiKey' => #{
+            path => "/apikeys/v2/orgs/:partyId/api-keys/:apiKeyId/status",
+            method => <<"PUT">>,
+            handler => 'swag_server_apikeys_api_keys_handler'
+        },
+        'RevokeApiKey' => #{
+            path => "/apikeys/v2/orgs/:partyId/revoke-api-key/:apiKeyId",
+            method => <<"GET">>,
+            handler => 'swag_server_apikeys_api_keys_handler'
+        }
+    }.
